dc6af2b1072bbba5f31f6ed18033d53fc00a3e15
stalasila20 <stalasila20@gmail.com>
2021-04-21 14:46:42 -0400

    Interest Update

--- Sowjanya Work/src/practices/CalculateInterest.java
+++ Sowjanya Work/src/practices/CalculateInterest.java
@@ -2,8 +2,13 @@ package practices;
 
 public class CalculateInterest {
     public static void main(String[] args) {
-        for(int i = 0; i < 9; i++){
-            System.out.println("CaluclateInterest");
+        for(int i = 2; i < 9; i++){
+            System.out.println("10000 at "+ i +"% interest = " + calculateInterest(10000.0, i));
         }
+    }
+
+    public  static double calculateInterest(double amount, double interestRate){
+        //System.out.println((interestRate/100));
+        return(amount *(interestRate/100));
     }
 }
