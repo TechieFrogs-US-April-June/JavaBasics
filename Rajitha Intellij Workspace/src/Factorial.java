79e76e39987a57d58b79771f24e7f4eb9cfff2a4
RajithaRaniChakilam <rajitha21@gmail.com>
2021-04-21 14:18:32 +0530

    commit from intellij

--- Rajitha Intellij Workspace/src/Factorial.java
+++ Rajitha Intellij Workspace/src/Factorial.java
@@ -6,7 +6,7 @@ public class Factorial {
         int f=1;
         for(int i=1;i<=n;i++) // 1<=5  2<=5 3<=5 4<=5 5<=5
             f=f*i;//f=1*1=1 f=1*2=2 f=2*3=6 f=6*4=24 f=24*5=120
-
+System.out.println("Program for factorial");
         System.out.print(f);
         /* int i=1;
         while(i<=n)
