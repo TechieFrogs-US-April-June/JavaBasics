60e8a1b21037bbc74fed06f5688034b9bd37b363
SangeethaMahendra <s.mahendran.bee@gmail.com>
2021-04-21 17:59:53 -0400

    Sangeetha's Commit

--- SangeethaMahendra/src/AssignmentsUntilConditional/SumOfNaturalNumbers.java
+++ SangeethaMahendra/src/AssignmentsUntilConditional/SumOfNaturalNumbers.java
@@ -5,11 +5,11 @@ public class SumOfNaturalNumbers {
         int n = 10;
         int n1 = 1;
         int i;  
-        for (i = 0; i<=n; i++)
+        for (i = 0; i<=n; i++) //0<=10; post increment 
         {
-            n1 = n1 + i;
-            System.out.println(i);
-            System.out.println("Factorial of a Number " + n1);
+            n1 = n1 + i;  // 1=1+0; 2=1+1; 4=2+2
+            System.out.println(i); // i=0; i=1; i=2
+            System.out.println("Sum of Natural Number " + n1); //n1=1; n1=2; n1=4
             
         }
         
