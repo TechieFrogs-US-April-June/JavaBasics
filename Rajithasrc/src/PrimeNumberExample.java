bb09df7d427b2c8f6946166b260e7183282bcfa3
RajithaRaniChakilam <82829932+RajithaRaniChakilam@users.noreply.github.com>
2021-04-21 07:06:41 +0530

    Update PrimeNumberExample.java
    committing from repo

--- Rajithasrc/src/PrimeNumberExample.java
+++ Rajithasrc/src/PrimeNumberExample.java
@@ -1,5 +1,5 @@
 class PrimeNumberExample{
     public static void main(String[] args){
-        System.out.println("This is a prime program");
+        System.out.println("This is not a prime program");
     }
 }
