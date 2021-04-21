c46bf2b99917e8e110e0b2fa0b2f4207778a4bdc
sirishagummadi900 <sirishagummadi900@gmail.com>
2021-04-21 05:52:43 -0700

    FirstCommit

---
+++ SirishaWork/src/checkAlphabet.java
@@ -0,0 +1,10 @@
+public class checkAlphabet {
+    public static void main(String[] args) {
+        char c = 'q';
+
+        if ((c>='a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            System.out.println( c + " is an alphabet");
+        else
+            System.out.println(c + " is not an alphabet");
+    }
+}
