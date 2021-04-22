e23fd95c68d23d0c267b33892e082b69de443d36
prathyushacodes <prathyushaeerpini@gmail.com>
2021-04-21 17:33:45 -0700

    Adding comment

--- PrathyushaCodes/src/Factorial.java
+++ PrathyushaCodes/src/Factorial.java
@@ -4,6 +4,7 @@ public class Factorial {
         int input = 4;
         for (int i=1; i<=input; i++) 
         {
+          // Calculating factorial
           factorialvalue=factorialvalue*i; 
         }
         System.out.println("The factorial of "+input+ " is "+ factorialvalue);
