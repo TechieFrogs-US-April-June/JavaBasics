1e62e264fdc6e690d77cb22c9f29dce7077e8ca9
gaihub1 <82833933+gaihub1@users.noreply.github.com>
2021-04-22 01:44:36 +0100

    Add files via upload

---
+++ VowelOrConsonantPgm.java
@@ -0,0 +1,9 @@
+public class VowelOrConsonantPgm {
+    {
+        char ch = 'G';
+        if (ch == 'a'|| ch == 'e'||ch == 'i'||ch == 'o'||ch == 'u') {
+            System.out.println(ch + " is vowel");
+        } else
+            System.out.println(ch + " is consonant");
+    }
+}
