9707b45ad3c200fba9098c5e2f4445c80a1e4a82
PrathyushaCodes <82817290+PrathyushaCodes@users.noreply.github.com>
2021-04-21 17:34:49 -0700

    Adding comment from github

--- PrathyushaCodes/src/Factorial.java
+++ PrathyushaCodes/src/Factorial.java
@@ -5,6 +5,7 @@ public class Factorial {
         for (int i=1; i<=input; i++) 
         {
           // Calculating factorial
+          // Adding comment directly from github
           factorialvalue=factorialvalue*i; 
         }
         System.out.println("The factorial of "+input+ " is "+ factorialvalue);
