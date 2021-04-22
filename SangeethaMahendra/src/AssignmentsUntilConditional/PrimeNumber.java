93cf08f54bb843962652749bc070666dace2a72e
SangeethaMahendra <s.mahendran.bee@gmail.com>
2021-04-21 23:16:45 -0400

    Still working

--- SangeethaMahendra/src/AssignmentsUntilConditional/PrimeNumber.java
+++ SangeethaMahendra/src/AssignmentsUntilConditional/PrimeNumber.java
@@ -1,8 +1,27 @@
-class PrimeNumber{
-    public static void main(String[] args) {
-        
-        System.out.println("prime number");
-        System.out.println("This is not a prime number");
-        System.out.println("not prime number");
+class PrimeNumber
+{
+    public static void main(String[] args)
+    {
+        for (int i=2; i<=21; i++) // 2<=21; yes ; Post increment
+        {
+            if( i == 2 || i == 3) // 2 == 2; 
+            {
+                System.out.println(i +" is not a prime number.");
+            }
+           else 
+           {
+               if((i%2 != 0) && i%3 != 0)
+               {
+               System.out.println(i+" is a prime number.");
+               }
+               //else{
+                //System.out.println(i +" is not a prime number.");
+               //}
+           }
+        }
     }
-}
+       /* System.out.println("prime number");
+        System.out.println("This is not a prime number");
+        System.out.println("not prime number");*/
+ }
+
