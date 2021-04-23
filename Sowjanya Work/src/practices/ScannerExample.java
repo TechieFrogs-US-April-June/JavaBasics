9230b708bb1e6c9c7105185e5572a77a3d89da4c
stalasila20 <stalasila20@gmail.com>
2021-04-22 20:25:10 -0400

    Scanner Programs

--- Sowjanya Work/src/practices/ScannerExample.java
+++ Sowjanya Work/src/practices/ScannerExample.java
@@ -6,18 +6,19 @@ public class ScannerExample {
     public static void main(String[] args) {
         Scanner vm = new Scanner(System.in);
         System.out.print("Enter your name: ");
-        //System.out.print("Enter your age: ");
-        //System.out.println("Enter your gender: ");
-        //System.out.println("Enter your ph: ");
         String name = vm. nextLine();
-        //int age = vm.nextInt();
-        //char gender = vm.next().charAt(0);
-        //long ph = vm.nextLong();
+        System.out.print("Enter your age: ");
+        int age = vm.nextInt();
+        System.out.print("Enter your gender: ");
+        char gender = vm.next().charAt(0);
+        System.out.print("Enter your ph: ");
+        long ph = vm.nextLong();
 
         System.out.println("Name is: " +name);
-        //System.out.println("Age is: " +age);
-        //System.out.println("Gender is: " +gender);
-        //System.out.println("Phone number is: " +ph);
+        System.out.println("Age is: " +age);
+        System.out.println("Gender is: " +gender);
+        System.out.println("Phone number is: " +ph);
+        vm.close();
 
     }
 }
