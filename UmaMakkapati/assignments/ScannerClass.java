b8e835a15be87f48ef8da17f87e277bb512cc7ca
umamk <umaitr123@gmail.com>
2021-04-23 19:04:25 -0500

    ScannerUpdate

--- UmaMakkapati/assignments/ScannerClass.java
+++ UmaMakkapati/assignments/ScannerClass.java
@@ -11,13 +11,22 @@ public class ScannerClass {
 		System.out.println("Take output from user ");
 		//a = sc.nextInt();//prints only integer values only
 		
-		String str1, str2;
+		String str1 = "uma" ;
+		String str2 = "4568";
+		if(sc.hasNext()) {
+				str1 = sc.next();
+				System.out.println("It has integer");
+				}
+				else {
+					System.out.println("Integer is not there");
+				}
 		//str = sc.nextLine();//prints whole message
 		str1 = sc.next();//take one word from sentence as token
 		str2 = sc.next();
 		System.out.println("First value is "+str1+" Second value is "+str2);
 		System.out.println("Delimeter: "+sc.delimiter());
 		sc.close();
+	}}
 		
 		/*float b;
 		b = sc.nextFloat();
@@ -35,6 +44,6 @@ public class ScannerClass {
 		sc.close();*/
 		
 
-	}
+	
 
-}
+
