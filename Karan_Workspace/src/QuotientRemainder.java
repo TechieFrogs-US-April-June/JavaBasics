8afa1d037c1ec8d0607afa4ca2050c75a3cb3a44
karandavuluri <karansri32@gmail.com>
2021-04-23 19:28:27 -0500

    more validations updated

--- Karan_Workspace/src/QuotientRemainder.java
+++ Karan_Workspace/src/QuotientRemainder.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
 
 public class QuotientRemainder {
+    static int a,b;
     public static void main(String[] args) {
         
         Scanner scan = new Scanner(System.in);
         System.out.println("enter Dividend :: ");
-        int a = scan.nextInt();
+        if(scan.hasNextInt()){
+            a = scan.nextInt();}
+        else {System.out.println("Please enter INTEGER value");System.exit(1);}
         System.out.println("enter Divisor :: ");
-        int b = scan.nextInt();
-        scan.close();
+        if(scan.hasNextInt()){
+            b = scan.nextInt();}
+        else {System.out.println("Please enter INTEGER value");System.exit(1);}        scan.close();
         int QUOTIENT = a/b;
         System.out.println("The Quotient result is ::  " + QUOTIENT);
         int REMAINDER = a%b;
