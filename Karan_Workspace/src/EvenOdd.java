8d76549a26bff8ff9698e51495fa2d7a37f67d6d
karandavuluri <karansri32@gmail.com>
2021-04-23 19:02:16 -0500

    updated some validations

--- Karan_Workspace/src/EvenOdd.java
+++ Karan_Workspace/src/EvenOdd.java
@@ -4,9 +4,13 @@ public class EvenOdd {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println(" Enter the number to check for Even/Odd :: ");
+        if(scan.hasNextInt()){
         int a = scan.nextInt();
         scan.close();
         if (a%2 == 0) {System.out.println(" The number entered is EVEN....");}
             else{System.out.println(" The number entered is ODD!!!!");}
+        }
+        else {System.out.println("please enter INTEGER value");}
+        
     }
 }
