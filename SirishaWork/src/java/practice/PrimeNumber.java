a1d6def463b09904c1217898c5a815f2e75ea7d4
sirishagummadi900 <sirishagummadi900@gmail.com>
2021-04-25 08:13:03 -0700

    2nd Exercise

---
+++ SirishaWork/src/java/practice/PrimeNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int temp;
+        boolean isPrime =true;
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Enter any number:");
+        //capture the input in an integer
+        int num=scan.nextInt();
+        scan.close();
+        int i=2;
+        while(i<= num/2)
+        {
+            if(num % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+            i++;
+        }
+        //If isPrime is true then the number is prime else not
+        if(isPrime)
+            System.out.println(num + " is a Prime Number");
+        else
+            System.out.println(num + " is not a Prime Number");
+    }
+    }
