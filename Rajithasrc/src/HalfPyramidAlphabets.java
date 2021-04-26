8ec283f0864240f9dfbf3d4275bed305192158cf
RajithaRaniChakilam <rajitha21@gmail.com>
2021-04-26 09:45:35 +0530

    commitiing changes

---
+++ Rajithasrc/src/HalfPyramidAlphabets.java
@@ -0,0 +1,15 @@
+public class HalfPyramidAlphabets {
+    public static void main(String[] args) {
+        char a = 'A';
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(a + " ");
+            }
+            a++;
+
+            System.out.println();
+        }
+    }
+    
+}
