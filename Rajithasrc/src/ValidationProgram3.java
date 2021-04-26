704f267fb151d118401665e5f49e463e631ec419
RajithaRaniChakilam <rajitha21@gmail.com>
2021-04-26 09:43:24 +0530

    committing changes

---
+++ Rajithasrc/src/ValidationProgram3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ValidationProgram3 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the string for name");
+        String str= sc.next();
+
+        
+
+        sc.close();
+    }
+    
+}
