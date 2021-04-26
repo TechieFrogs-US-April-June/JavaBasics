61fc82ddf0c02e6f26c2df6ea89f80da3140fe55
karandavuluri <karansri32@gmail.com>
2021-04-26 02:48:10 -0500

    added validations

--- Karan_Workspace/src/VowelConsonant.java
+++ Karan_Workspace/src/VowelConsonant.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-
 public class VowelConsonant {
     static boolean x=true;
+    static String len;
+    public static Scanner scan = new Scanner(System.in);
 
     static void alpha() {
         while(x){
-        Scanner scan = new Scanner(System.in);
         System.out.println(" enter to check if consonent / Vowel :: ");
-        String len = scan.next();
+        len = scan.next();
 
         if(len.length()==1){
         break;
@@ -16,16 +16,31 @@ public class VowelConsonant {
         System.out.println("Please enter only ONE character");
         }
     }
+
+    static boolean numcheck()
+    {
+        try {
+            Integer.parseInt( len );
+            return true;
+        }
+        catch( Exception e ) {
+            return false;
+        }
+    }
+        
     public static void main(String[] args) {
+
         while(x){
         alpha();
-        Scanner scan = new Scanner(System.in);
-        System.out.println(" Enter an Alphabet to check if Consonent / Vowel :: ");
-        char a = scan.next().charAt(0);
-        if(a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' )
-            {System.out.println("entered alphabet  " +a  + "  is vowel");
+        numcheck();
+        if (numcheck()==false){
+            char a = len.charAt(0);
+            if(a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' )
+            {System.out.println("entered alphabet  " +a  + "  is vowel");}
+        else{ System.out.println("entered alphabet  " +a  + "  is consonant");}
         break;}
-        System.out.println("entered alphabet  " +a  + "  is consonant");
+        System.out.println("You have entered a NUMBER. So please enter an ALPHABET");
+        
     }
         
     }
