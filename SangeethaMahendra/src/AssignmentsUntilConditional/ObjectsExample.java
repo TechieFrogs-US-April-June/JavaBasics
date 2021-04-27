c75a0be0e5c57028a695f24886cdbd5d79ab2eb1
SangeethaMahendra <s.mahendran.bee@gmail.com>
2021-04-26 21:03:34 -0400

    Sangeetha's Commit

---
+++ SangeethaMahendra/src/AssignmentsUntilConditional/ObjectsExample.java
@@ -0,0 +1,31 @@
+public class ObjectsExample {
+
+    int a = 5; // initializing instance variable
+    int b = 10;
+    static int i; // static variable
+    void display()
+    {
+        System.out.println("Values of a and b "+ a + " "+ b);
+    }
+    public static void main(String[] args) {
+         //craeting object for the initialized instance variable
+         //  1           2     3       4
+        ObjectsExample objEg = new ObjectsExample(); // 1 = Class; 2 = object reference with a new keyword; 3 = new keyword , allocates memory; 4 = constructor; Constructor must be same name as class name
+        objEg.display();
+        objEg.a = 10;
+        objEg.b = 20;
+        objEg.display();
+
+        ObjectsExample objEg2 = new ObjectsExample();
+        objEg2.a =100;
+        objEg2.b = 200;
+        objEg2.display();
+        objEg.display(); // updated the value to 10 and 20; the output for this is 10 and 20
+
+        System.out.println(i); // No need to create object for static variable
+
+        System.out.println(objEg.i); //proper way of use it
+
+    }
+    
+}
--- SangeethaMahendra/src/AssignmentsUntilConditional/ScannarSkip.java
+++ SangeethaMahendra/src/AssignmentsUntilConditional/ScannarSkip.java
@@ -2,21 +2,37 @@ import java.util.Scanner;
 
 public class ScannarSkip {
     public static void main(String[] args) {
-      System.out.println("Enter a String Value");
-        Scanner scan = new Scanner("Welcome to Java Welcome");
-   //     String fullName = scan.next();
+      String s = "Hello World! 3 + 3.0 = 6.0 true ";
+
+      // create a new scanner with the specified String Object
+      Scanner scanner = new Scanner(s);
+
+      // skip the word "Hello"
+      scanner.skip("Hello");
+
+      // print a line of the scanner
+      System.out.println("" + scanner.nextLine());
+
+      // close the scanner
+      scanner.close();
+
+
+
+     String a = "Hello Welcome to Java Welcome";
+        Scanner scan = new Scanner(a);
        scan.skip("Hello");
+       System.out.println("" + scan.nextLine());
+       scan.close(); 
 
-       System.out.println(scan.nextLine());
-        
 
-    /*  System.out.println("Enter a String Value");
+    /*  Wrong Method
+     System.out.println("Enter a String Value");
         Scanner s = new Scanner(System.in);
         String fullName = s.nextLine();
         s.skip(s.next());
         System.out.println(fullName);*/
 
-        scan.close();
+       // scan.close(); 
        //s.close();
 
     }
