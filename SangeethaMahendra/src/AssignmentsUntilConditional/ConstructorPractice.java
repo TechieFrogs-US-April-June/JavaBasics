5a50f76c7ed919e48ce33f688917964226424471
SangeethaMahendra <s.mahendran.bee@gmail.com>
2021-04-28 22:02:54 -0400

    Sangeetha's Commit 2/28/21

---
+++ SangeethaMahendra/src/AssignmentsUntilConditional/ConstructorPractice.java
@@ -0,0 +1,20 @@
+public class ConstructorPractice {
+    
+    int a = 5;
+    int b = 10;
+    
+    ConstructorPractice(){
+
+    }
+    
+    
+    public static void main(String[] args) {
+    
+     ConstructorPractice constructorObj = new ConstructorPractice(); //default constructor
+     // constructorObj.a; // not accepting
+    System.out.println(constructorObj.a); // accepted syntax
+    System.out.println(constructorObj.b); 
+
+    }
+    
+}
