ea148e47bde7c4909e9479ad865e459aa2ea9fef
SangeethaMahendra <s.mahendran.bee@gmail.com>
2021-05-12 23:20:29 -0400

    Sangeetha's Commit

--- SangeethaMahendra/src/Assignments/FloydsTriangle.java
+++ SangeethaMahendra/src/Assignments/FloydsTriangle.java
@@ -1,17 +1,19 @@
 package src.Assignments;
-// not yet finished
+//  finished
 public class FloydsTriangle {
     public static void main(String[] args) {
+        int k=1;
         //Rows
         for (int i = 1; i <= 5; i++) { 
 
             //Spaces
             for (int j = 1; j <= i; j++) {
-                System.out.print("");
-            }
+               // System.out.print("");
+          
                // Printing numbers
-              for(int k=1; k<=i; k++){
-                System.out.print(""+ k);
+              
+                System.out.print(k+" ");
+                k++;
                
             }
             System.out.println();
