06ab09b6df56d552c4313016fecebe9580e250d9
umamk <82835165+umamk@users.noreply.github.com>
2021-05-14 15:58:22 -0500

    New one

---
+++ UmaMakkapati/assignments/PrimeOrNot14.java
@@ -0,0 +1,25 @@
+package com.BasicSamples;
+
+public class PrimeOrNot14 {
+			int i;
+			static int num;
+				
+	public static void main(String[] args) {
+	
+		int i=2;
+		while(i<= num/2)
+		{
+		   if(num % i == 0)
+		   {
+			boolean isPrime = false;
+			break;
+		   }
+		   i++;
+		}
+	
+
+	}
+}
+	
+
+	
